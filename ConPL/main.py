a9ef4633d7199018b9d866fbd89901076f7d6bfc
thanhnx12 <thanhnx1209@gmail.com>
2024-04-08 19:10:37 +0700

    fix infonce

--- ConPL/main.py
+++ ConPL/main.py
@@ -310,8 +310,11 @@ def train_model_with_hard_neg(config, model, mem_set, traindata, epochs, current
                     f_pos = model.sentence_encoder.infoNCE_f(lmhead_output[i],rep[i] , temperature = config['infonce_temperature'])
                     f_neg = model.sentence_encoder.infoNCE_f(lmhead_output[i],neg_prototypes , temperature = config['infonce_temperature'])
                     f_concat = torch.cat([f_pos.unsqueeze(0),f_neg],dim = 0)
+                    #quick fix for large number
+                    f_concat = torch.log(torch.max(f_concat, torch.tensor(1e-9).to(config['device'] )))
 
-                    infoNCE_loss += -torch.log(softmax(f_concat/abs(f_concat).max())[0])
+
+                    infoNCE_loss += -torch.log(softmax(f_concat )[0])
             except Exception as e:
                 print(e.with_traceback())
                 print("no infoNCE_loss")
@@ -457,7 +460,10 @@ def train_memory(config, model, mem_set, train_set, epochs, current_proto, origi
                     f_neg = model.sentence_encoder.infoNCE_f(lmhead_output[i],neg_prototypes , temperature = config['infonce_temperature'])
                     f_concat = torch.cat([f_pos.unsqueeze(0),f_neg],dim = 0)
 
-                    infoNCE_loss += -torch.log(softmax(f_concat/abs(f_concat).max())[0])
+                    #quick fix for large number
+                    f_concat = torch.log(torch.max(f_concat, torch.tensor(1e-9).to(config['device'] )))
+
+                    infoNCE_loss += -torch.log(softmax(f_concat)[0])
             except Exception as e:
                 print(e.with_traceback())
                 print("no infoNCE_loss")
@@ -560,10 +566,8 @@ if __name__ == '__main__':
     epochs = 1
     threshold=0.1
 
-    
-
     wandb.init(
-    project = 'CRE-via-MMI',
+    project = 'DATN',
     name = f"ConPL_{args.task}_{args.shot}_{config['infonce_lossfactor']}_{config['mlm_lossfactor']}",
     config = {
         'name': "ConPL",
